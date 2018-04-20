4512c33d6be9cd30e02faa3b7b5a1d30ef8bb881
Lu-cindy <ya850310@gmail.com>
2018-04-20 11:04:11 +0800

    5.fort

---
+++ Assets/move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("left") || Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -180 * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey("right") || Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 180 * Time.deltaTime, 0);
+        }
+	}
+}
